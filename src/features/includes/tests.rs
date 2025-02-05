a7443659ccb375df464cf745dde73d9144a67792
deniz.hofmeister <deniz.hofmeister@gmail.com>
2025-02-05 17:25:10 +0100

    include_base test added

--- src/features/includes/tests.rs
+++ src/features/includes/tests.rs
@@ -1,10 +1,14 @@
 #[cfg(test)]
 mod macro_tests {
-    use crate::features::macros::MacroProcessor;
+    use crate::{features::macros::MacroProcessor, XacroProcessor};
     #[test]
     fn test_include_basic() {
         let macro_processor = MacroProcessor::new();
+        let data = XacroProcessor::parse_file("tests/data/include_base.xacro").unwrap();
+        let expected = XacroProcessor::parse_file("tests/data/include_base_expected.urdf").unwrap();
+
         let result = macro_processor.process(data);
+
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), expected);
     }
